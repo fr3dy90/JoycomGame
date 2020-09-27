3a41684076ae0ed800a9aa054c18053a76d0cae0
Fredy <jfr3dy90@gmail.com>
2020-09-27 10:32:57 -0500

    Basic Player Car

---
+++ CarTest/Assets/Project/Script/Wheel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+   public WheelCollider wc;
+   public GameObject wheelMesh;
+   public bool canSteering;
+   public Transform skidTrail;
+   public ParticleSystem skidSmoke;
+
+   private void Start()
+   {
+      wc = GetComponent<WheelCollider>();
+      if (wc == null)
+      {
+         Debug.LogWarning("Missing wheel collider on:" + transform.name);
+      }
+   }
+}
